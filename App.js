bb6dc6fba4f8c60ef56b069b429f695d74fe4978
C.T <ct.bell@yahoo.com>
2021-11-24 15:19:04 -0500

    Adding setup files

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  const [response, setResponse] = useState("noValue");
+
+  const fetchEndpoint = async () => {
+    const response = await fetch("https://hello.world/", {
+      method: "GET",
+    });
+    const resData = await response.json();
+
+    console.log("resData ", resData);
+    setResponse(resData);
+    return;
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button title="Press Me" onPress={fetchEndpoint} />
+      <Text>{JSON.stringify(response)}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ App.test.js
@@ -0,0 +1,53 @@
+import { render, act, fireEvent } from "@testing-library/react-native";
+import React from "react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+import App from "./App";
+
+const server = setupServer(
+  // Describe the requests to mock.
+  rest.get("https://hello.world/", (req, res, ctx) => {
+    console.log("Endpoint Hit");
+    return res(
+      ctx.json({
+        title: "Lord of the Rings",
+        author: "J. R. R. Tolkien",
+      })
+    );
+  })
+);
+beforeAll(() => {
+  // Establish requests interception layer before all tests.
+  server.listen({
+    onUnhandledRequest(req) {
+      console.error(
+        "Found an unhandled server request while testing",
+        req.method,
+        req.url.href
+      );
+    },
+  });
+});
+afterAll(() => {
+  // Clean up after all tests are done, preventing this
+  // interception layer from affecting irrelevant tests.
+  server.close();
+});
+test("renders a book data", () => {
+  // Render components, perform requests, API communication is covered.
+});
+
+it("Intercepts the api call successfully", async () => {
+  const { getByText, findByText } = render(<App />);
+
+  const button = getByText("Press Me");
+  fireEvent.press(button);
+
+  await findByText(
+    JSON.stringify({
+      title: "Lord of the Rings",
+      author: "J. R. R. Tolkien",
+    })
+  );
+});
